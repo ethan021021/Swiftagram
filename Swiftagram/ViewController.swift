01ceb781bd6c819aee99a88d895743ef4d7b640f
Ethan Thomas <ethan021@me.com>
2016-08-29 04:01:33 -0700

    Initial Commit

---
+++ Swiftagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiftagram
+//
+//  Created by Ethan Thomas on 8/29/16.
+//  Copyright © 2016 Ethan Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
